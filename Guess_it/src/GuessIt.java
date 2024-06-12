9d7008b2c8444b56eec876c57a2b085aa20f3f47
Damz <dsethuge02@gmail.com>
2024-06-12 17:32:56 +0530

    Updating the code

--- Guess_it/src/GuessIt.java
+++ Guess_it/src/GuessIt.java
@@ -2,7 +2,41 @@ import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class GuessIt {
+    private static final int Min = 0;
+    private static final int Max = 100;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GuessIt play = new GuessIt();
+        play.startGame();
+    }
+
+    public void startGame() {
+        boolean UserGuessCorrect = false;
+        int guessNumberCount = 0;
+        int randomNumber = getNumber();
+
+        while (!UserGuessCorrect) {
+            int userNumber = getUserGuess();
+            if (userNumber > randomNumber) {
+                System.out.println("Sorry, the number you guessed is too high :(");
+            } else if (userNumber < randomNumber) {
+                System.out.println("Sorry, the number you guessed is too low :(");
+            } else {
+                System.out.println("Congratulations! Your guess is correct :)");
+                UserGuessCorrect = true;
+            }
+            guessNumberCount++;
+        }
+        System.out.println("You found the number in " + guessNumberCount + " guesses..... Well Done!");
+    }
+
+    public int getNumber() {
+        return ThreadLocalRandom.current().nextInt(Min, Max + 1);
+    }
+
+    public int getUserGuess() {
+        Scanner line = new Scanner(System.in);
+        System.out.println("Please guess the number: ");
+        return line.nextInt();
     }
 }
