5e17361ba774cb2a10247a39894136cd4cae7042
Damz <dsethuge02@gmail.com>
2024-06-12 17:15:38 +0530

    Game File Adding

---
+++ Guess_it/src/GuessIt.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class GuessIt {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
